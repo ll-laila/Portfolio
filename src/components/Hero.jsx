e6d56f4ff2c0167f505db942b951632ed53afb28
ll-laila <lailatimasli1@gmail.com>
2024-09-15 13:27:55 +0100

    update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Laila Timasli</span>
           </h3>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          computer engineer student <br className='sm:block hidden' />
+          computer engineer student |<br className='sm:block hidden' />
            Cloud | aws | IA | Developpement | Devops enthusiast      </p>
         </div>
       </div>
