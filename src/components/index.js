b5784dbb55abce71bcee3ed4805c40952cee370c
ll-laila <lailatimasli1@gmail.com>
2024-10-06 12:50:10 +0100

    resolve works probleme

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,8 @@ import Navbar from "./Navbar";
 import About from "./About";
 import Experience from "./Experience";
 import Works from "./Works";
+import Works2 from "./Works2";
+import Works3 from "./Works3";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
@@ -16,6 +18,8 @@ export {
   About,
   Experience,
   Works,
+  Works2,
+  Works3,
   Feedbacks,
   Contact,
   CanvasLoader,
